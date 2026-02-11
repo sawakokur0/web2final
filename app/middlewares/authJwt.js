940e0d5f47481b38e1c738a5d9520aed08564dd5
sawakokur0 <roota14344@gmail.com>
2026-02-12 04:49:55 +0500

    still fixing login

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -11,7 +11,7 @@ verifyToken = (req, res, next) => {
     return res.status(403).send({ message: "No token provided!" });
   }
 
-  jwt.verify(token, config.secret, (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ message: "Unauthorized!" });
     }
